3f93ef7016bebaaf3e6f37ca969599aeb9f83fe7
Allen Boynton <aboynton@fullsail.edu>
2015-06-10 15:49:31 -0400

    Created main window
    Created main window and nav window variables.

--- 1506_AboutMe_Boynton_Allen/Resources/app.js
+++ 1506_AboutMe_Boynton_Allen/Resources/app.js
@@ -3,3 +3,18 @@
 // About Me App
 // June 10, 2015
 
+// Background color
+Ti.UI.setBackgroundColor("#000");
+
+// Background
+var mainWindow = Ti.UI.createWindow ({
+	backgroundColor: "#fff",
+	title: "ABOUT ME"
+});
+
+// Convert to navigation window
+var navWindow = Ti.UI.iOS.createNavigationWindow ({
+	window: mainWindow
+});
+
+
