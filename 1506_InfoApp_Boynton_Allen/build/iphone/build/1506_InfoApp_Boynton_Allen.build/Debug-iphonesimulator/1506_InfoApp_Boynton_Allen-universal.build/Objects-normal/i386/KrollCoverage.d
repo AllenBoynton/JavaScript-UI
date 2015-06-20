97029da2c29ed92c5fb59cf46539c49931a4cd8a
Allen Boynton <aboynton@fullsail.edu>
2015-06-20 13:38:30 -0400

    Played with aesthetics

--- 1506_InfoApp_Boynton_Allen/build/iphone/build/1506_InfoApp_Boynton_Allen.build/Debug-iphonesimulator/1506_InfoApp_Boynton_Allen-universal.build/Objects-normal/i386/KrollCoverage.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aboynton/Desktop/MIU1506-Allen_Boynton/1506_InfoApp_Boynton_Allen/build/iphone/Classes/KrollCoverage.m
--- 1506_InfoApp_Boynton_Allen/build/iphone/build/1506_InfoApp_Boynton_Allen.build/Debug-iphonesimulator/1506_InfoApp_Boynton_Allen-universal.build/Objects-normal/x86_64/KrollPropertyDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/aboynton/Desktop/MIU1506-Allen_Boynton/1506_InfoApp_Boynton_Allen/build/iphone/Classes/KrollPropertyDelegate.m \
-  /Users/aboynton/Desktop/MIU1506-Allen_Boynton/1506_InfoApp_Boynton_Allen/build/iphone/Classes/KrollPropertyDelegate.h
